afae0547a5c23977084e61a6e2f24f3ae355e5ab
srini <srini903@gmail.com>
2019-09-10 08:13:40 -0700

    Authentication changes

---
+++ vet-clinic-app/src/main/java/com/vet/clinic/app/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.vet.clinic.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import com.vet.clinic.app.web.security.jwt.JwtSecurityConfigurer;
+import com.vet.clinic.app.web.security.jwt.JwtTokenProvider;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  JwtTokenProvider jwtTokenProvider;
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //@formatter:off
+      
+      if(false) {
+        //http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        
+        http.authorizeRequests()
+        .antMatchers("/veterinarians/**").hasRole("ADMIN")
+        .antMatchers("/").permitAll()
+        .antMatchers("/h2_console/**").permitAll();
+
+      http.csrf().disable();
+      http.headers().frameOptions().disable();
+        
+      }else {
+        
+        http
+        .httpBasic().disable()
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .authorizeRequests()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/h2_console/**").permitAll()
+            .antMatchers("/veterinarians/**").hasRole("ADMIN")
+            .antMatchers("/petOwners/**").permitAll()
+            .anyRequest().authenticated()
+        .and()
+        .apply(new JwtSecurityConfigurer(jwtTokenProvider));
+        
+        http.headers().frameOptions().disable();
+      }
+
+      
+
+
+        //@formatter:on
+    }
+
+
+}
+
