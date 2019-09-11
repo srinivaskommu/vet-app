d87f980448a526602f373619bb864149e336e1b2
srini <srini903@gmail.com>
2019-09-10 21:45:40 -0700

    deleting stuff

--- vet-clinic-webapp/webpack.config.dev.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const BaseConfig = require('./webpack.config.base');
-module.exports = merge(BaseConfig, {
-        mode: 'development',
-	devServer: {
-        port: 3030
-      }
-});
--- vet-clinic-webapp/webpack.config.production.js
+++
@@ -1,5 +0,0 @@
-const merge = require('webpack-merge');
-const BaseConfig = require('./webpack.config.base');
-module.exports = merge(BaseConfig, {
-        mode: 'production'
-});
